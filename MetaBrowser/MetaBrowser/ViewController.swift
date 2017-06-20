11781902f0c4ec4347d68ff2668a11e1865bb6d5
midmirror <midmirror@Mellows-MacBook-Pro.local>
2017-06-20 09:53:42 +0800

    Establish Project

---
+++ MetaBrowser/MetaBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetaBrowser
+//
+//  Created by midmirror on 17/6/20.
+//  Copyright © 2017年 midmirror. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
